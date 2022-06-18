8924a9c776759fe6989862b6b51c634ac1860a83
Ronnie Rios <ronniesmail14@gmail.com>
2022-06-18 13:39:07 -0500

    router installed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import AllPosts from './components/AllPosts';
+import SinglePost from './components/SinglePost';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-
+      <Router>
+        <Router path='/' element={<AllPosts />} />
+        <Router path='/' element={<SinglePost />} />
+      </Router>
     </div>
+    </Router>
   );
 }
 
