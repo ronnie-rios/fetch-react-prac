7aae02baca7126342ea7a5f3f9f13ca8405e7ce4
Ronnie Rios <ronniesmail14@gmail.com>
2022-06-18 13:55:50 -0500

    navbar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import AllPosts from './components/AllPosts';
 import SinglePost from './components/SinglePost';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBar from './components/ui/NavBar';
 
 function App() {
   return (
     <Router>
     <div className="App">
+      <NavBar />
       <Routes>
         <Route path='/' element={<AllPosts />} />
         <Route path='/:id' element={<SinglePost />} />
